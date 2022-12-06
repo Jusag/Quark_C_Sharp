ed7dc31183310e5f66542c1b5931f965d90a9da6
Julio Gonzalez <jusag87@gmail.com>
2022-12-06 02:30:29 -0300

    some exercises

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace testing
     {
         static void Main(string[] args)
         {
-            page_7_4 auxC = new page_7_4(); //Change CLASS for the number of the exercise
+            page_20_1 auxC = new page_20_1(); //Change CLASS for the number of the exercise
             auxC.exercise();
         }
     }
@@ -131,6 +131,188 @@ namespace testing
         }
     }
 
+    class page_13_1
+    {
+        private string name = "";
+        private int age, salary;
+        public void exercise()
+        {
+            Console.WriteLine("Please enter the name of the new employee");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter the age of the " + name);
+            string auxLocal = Console.ReadLine();
+            age = int.Parse(auxLocal);
+            Console.WriteLine("Finally enter the salary of " + name);
+            auxLocal = Console.ReadLine();
+            salary = int.Parse(auxLocal);
+            if (salary > 3000)
+            {
+                Console.WriteLine("The new employee " + name + " with age " + age + " must pay taxes because the salary is above 3000");
+            }
+            else
+            {
+                Console.WriteLine("The new employee " + name + " with age " + age + " don't pay taxes because the salary is under or equal to 3000");
+            }
+        }
+    }
+
+    class page_15_1
+    {
+        public void exercise()
+        {
+            Console.WriteLine("Enter 8 int number for the vector separated with ENTER key");
+            int[] vectorLocal = new int[8];
+            string auxControlstringEntered;
+            for (int i = 0; i < vectorLocal.Length; i++)
+            {
+                auxControlstringEntered = Console.ReadLine();
+                vectorLocal[i] = int.Parse(auxControlstringEntered);
+            }
+            Console.WriteLine();
+            Console.Write("The values of de vector are ");
+            for (int i = 0; i < vectorLocal.Length; i++)
+            {
+                if (i != 7)
+                {
+                    Console.Write(vectorLocal[i] + " ");
+                }
+                else
+                {
+                    Console.WriteLine(vectorLocal[i]);
+                }
+            }
+            Console.WriteLine();
+            int totalSum = 0;
+            for (int i = 0; i < vectorLocal.Length; i++)
+            {
+                totalSum += vectorLocal[i];
+                if (i == vectorLocal.Length - 1)
+                {
+                    Console.WriteLine("The sum of all values is " + totalSum);
+                }
+            }
+            Console.WriteLine();
+            totalSum = 0;
+            for (int i = 0; i < vectorLocal.Length; i++)
+            {
+                if (vectorLocal[i] > 36)
+                {
+                    totalSum += vectorLocal[i];
+                }
+                if (i == vectorLocal.Length - 1)
+                {
+                    Console.WriteLine("The sum of all values above 36 is " + totalSum);
+                }
+            }
+            Console.WriteLine();
+            totalSum = 0;
+            for (int i = 0; i < vectorLocal.Length; i++)
+            {
+                if (vectorLocal[i] > 50)
+                {
+                    totalSum++;
+                }
+                if (i == vectorLocal.Length - 1)
+                {
+                    Console.WriteLine("In the vector there are " + totalSum + " values above 50");
+                }
+            }
+        }
+    }
+
+
+    class page_16_1
+    {
+        public void exercise()
+        {
+            Console.WriteLine("Enter the number of total values for the vector");
+            string auxStringRead = Console.ReadLine();
+            int[] vector = new int[int.Parse(auxStringRead)];
+            int totalSum = 0;
+            Console.WriteLine("Enter the values for the vector separated with ENTER key");
+            for (int i = 0; i < vector.Length; i++)
+            {
+                auxStringRead = Console.ReadLine();
+                vector[i] = int.Parse(auxStringRead);
+                totalSum += vector[i];
+                if (i == vector.Length - 1)
+                {
+                    Console.WriteLine("The sum of all values is " + totalSum);
+                }
+            }
+        }
+    }
+
+    class page_20_1
+    {
+        string[] countrys;
+        int[] population;
+        public page_20_1() //constructor
+        {
+            countrys = new string[5];
+            population = new int[5];
+        }
+        public void exercise()
+        {
+            string auxControlString;
+            for (int i = 0; i < countrys.Length; i++)
+            {
+                Console.Write("Enter the country name: ");
+                countrys[i] = Console.ReadLine();
+                Console.Write("Enter the population of: " + countrys[i]+": ");
+                auxControlString = Console.ReadLine();
+                population[i] = int.Parse(auxControlString);
+            }
+            //Name Control
+            for (int i = 0; i < countrys.Length; i++)
+            {
+                for (int j = 0; j < countrys.Length - i ; j++)
+                {
+                    if (j < countrys.Length - 1)
+                    {
+                        if (string.Compare(countrys[j], countrys[j + 1]) == 1)
+                        {
+                            string auxCountry = countrys[j];
+                            int auxPopulation = population[j];
+                            countrys[j] = countrys[j + 1];
+                            population[j] = population[j + 1];
+                            countrys[j + 1] = auxCountry;
+                            population[j + 1] = auxPopulation;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Alphabetical order ");
+            for (int i = 0; i < countrys.Length; i++)
+            {
+                Console.Write(countrys[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < countrys.Length; i++)
+            {
+                for (int j = 0; j < countrys.Length - i ; j++)
+                {
+                    if (j < countrys.Length - 1)
+                    {
+                        if (population[j] > population[j + 1])
+                        {
+                            string auxCountry = countrys[j];
+                            int auxPopulation = population[j];
+                            countrys[j] = countrys[j + 1];
+                            population[j] = population[j + 1];
+                            countrys[j + 1] = auxCountry;
+                            population[j + 1] = auxPopulation;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Population order");
+            for (int i = 0; i < countrys.Length; i++)
+            {
+                Console.Write(countrys[i] + " ");
+            }
+        }
+    }
 
 
 
